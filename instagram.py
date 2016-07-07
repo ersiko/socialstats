33d879ce573529c474cf89950b743f0c83f7f869
Tomàs Núñez <tomas@tomas.cat>
2016-07-08 01:20:10 +0200

    Fixes

--- instagram.py
+++ instagram.py
@@ -35,26 +35,38 @@ with open(dbFilePath,'r') as dbFile:
 
 most_liked={}
 now = datetime.date.today().strftime('%Y%m%d')
+cursor=0
+while cursor < 2:
+    print(data['entry_data']['ProfilePage'][0]['user']['media']['page_info']['end_cursor'])
 
-for pic in data['entry_data']['ProfilePage'][0]['user']['media']['nodes']:
-    likecount = pic['likes']['count']
-    picid = pic['code']
-    daycountobj = {'date': now, 'likes': likecount}
-    try:
-        if fotos[picid]['likecount'][-1]['date'] == now:
-            pass
-        else:
-            fotos[picid]['likecount'].append(daycountobj)
-        
+    for pic in data['entry_data']['ProfilePage'][0]['user']['media']['nodes']:
+        likecount = pic['likes']['count']
+        picid = pic['code']
+        daycountobj = {'date': now, 'likes': likecount}
         try:
-            most_liked[picid] = fotos[picid]['likecount'][-1]['likes'] - fotos[picid]['likecount'][-2]['likes']
-        except IndexError:
-            most_liked[picid] = fotos[picid]['likecount'][-1]['likes']
-    except KeyError:
-        fotos[picid] = {'caption' : pic['caption'], 'dateposted' : pic['date'], 'likecount': [daycountobj], 'fullpic': pic['display_src'], 'thumbnail': pic['thumbnail_src']}
-        most_liked[picid] = likecount
+            if fotos[picid]['likecount'][-1]['date'] == now:
+                pass
+            else:
+                fotos[picid]['likecount'].append(daycountobj)
+        
+            try:
+                most_liked[picid] = fotos[picid]['likecount'][-1]['likes'] - fotos[picid]['likecount'][-2]['likes']
+            except IndexError:
+                most_liked[picid] = fotos[picid]['likecount'][-1]['likes']
+        except KeyError:
+            fotos[picid] = {'caption' : pic['caption'], 'dateposted' : pic['date'], 'likecount': [daycountobj], 'fullpic': pic['display_src'], 'thumbnail': pic['thumbnail_src']}
+            most_liked[picid] = likecount
+    url = "https://www.instagram.com/alquintopino/?max_id=" +data['entry_data']['ProfilePage'][0]['user']['media']['page_info']['end_cursor']
+    cursor+=1
+    print(cursor,data['entry_data']['ProfilePage'][0]['user']['media']['page_info']['end_cursor'] )
+    r = requests.get(url)
+    p = bs(r.content,"html.parser")
+    for script in p.find_all('script'):
+        if 'window._sharedData' in script.text:
+            data=json.loads(script.text[20:-1])
 
 most_liked_sorted = sorted(most_liked.items(), key=itemgetter(1), reverse=True)
+
 i=0
 message="Veamos tus likes desde ayer!\n\n"
 
@@ -75,16 +87,10 @@ else:
     bot.send_message(CHAT_ID, message, parse_mode='Markdown')
 #    print(message)
 
-#print(json.dumps(fotos,indent=2))
-#    print(''.join(pic['caption'][:25].splitlines()) + ": " + str(pic['likes']['count']))
-#sorted_output = sorted(output.items(), key=lambda x: x[1]['viewcount'],reverse=True)
+try:
+    with open(dbFilePath,'w') as dbFile:
+        json.dump(fotos,dbFile)
+except:
+    print ("There was a problem writing the dbfile. Check permissions or disk space.")
 
-#for pic in sorted_output[:5]:
-#    type(pic)
-#    print("La foto '" + pic[1]['caption'] + "'(https://instagram.com/p/"+ pic[0] + ") tiene " + str(pic[1]['viewcount']) + " likes.")
-
-#try:
-#    with open(dbFilePath,'w') as dbFile:
-#        json.dump(fotos,dbFile)
-#except:
- #   print ("There was a problem writing the dbfile. Check permissions or disk space.")
+ 
