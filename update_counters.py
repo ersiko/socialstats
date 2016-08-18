314a071030c2a0688cb805a0e6e46da3e3cebd44
Tomàs Núñez <tomas@tomas.cat>
2016-08-18 21:48:20 +0200

    Testing the period data and it works. It can be heavily optimized, though. I'll check on that.

--- update_counters.py
+++ update_counters.py
@@ -39,21 +39,22 @@ for iguser in res['hits']['hits']:
         else:
             my_followers_update[period] = my_follows[-1]['_source']['followers'] - my_follows[0]['_source']['followers']
             my_following_update[period] = my_follows[-1]['_source']['following'] - my_follows[0]['_source']['following']
-        update = es.update(index="igusers", doc_type="following_diffs", id=iguser['_id'], body={"doc":my_following_update,'doc_as_upsert':True})
-        update = es.update(index="igusers", doc_type="followers_diffs", id=iguser['_id'], body={"doc":my_followers_update,'doc_as_upsert':True})
+    update = es.update(index="igusers", doc_type="following_diffs", id=iguser['_id'], body={"doc":my_following_update,'doc_as_upsert':True})
+    update = es.update(index="igusers", doc_type="followers_diffs", id=iguser['_id'], body={"doc":my_followers_update,'doc_as_upsert':True})
 
     data, maxid = igscrape.get_iguser_data(iguser['_id'], 2)
     for pic in data['entry_data']['ProfilePage'][0]['user']['media']['nodes']:
         pic_id = pic['code']
+        print("Foto: " +pic_id)
         my_likes_update = {}
         for period in ['1','3','7','30','90','180','365']:
-            my_likes=es.search(index="picsdaily-last-" + period + "-days",doc_type="likes",q="_id:"+iguser['_id'])['hits']['hits']
-            if len(my_follows) == 1:
+            my_likes=es.search(index="picsdaily-last-" + period + "-days",doc_type="likes",q="_id:"+pic_id)['hits']['hits']
+            if len(my_likes) == 1 or len(my_likes) == 0:
                 # La foto es vieja y se acaba de añadir. No tenemos datos de ayer. Mañana podremos calcular.
                 break
             else:
                 my_likes_update[period] = my_likes[-1]['_source']['number'] - my_likes[0]['_source']['number']
-        update = es.update(index='pics', doc_type='likes_diffs', id=pic_id, body={"doc":my_likes_update},'doc_as_upsert':True})
+        update = es.update(index='pics', doc_type='likes_diffs', id=pic_id, body={"doc":my_likes_update,'doc_as_upsert':True})
 
 
 
